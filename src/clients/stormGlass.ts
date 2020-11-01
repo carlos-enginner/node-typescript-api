94607c8259365969b2b0a224fd0a8c07d068a0ce
Carlos Augusto <gyn.carlos@gmail.com>
2020-11-01 15:20:18 -0300

    Instação do axios, criação do mock para teste da api stormGlass

---
+++ src/clients/stormGlass.ts
@@ -0,0 +1,16 @@
+import {AxiosStatic} from 'axios';
+
+export class StormGlass {
+    readonly apiParams = 
+    'swellDirection,swellHeight,swellPeriod,waveDirection,waveHeight,windDirection';
+
+    readonly source = 'noaa';
+
+    constructor(protected request: AxiosStatic) {}
+
+    public async fetchPoints(lat:number,lng:number) : Promise<{}> {
+        return this.request.get(
+            `https://api.stormglass.io/v2/weather/point?params=${this.apiParams}&source=${this.source}&end=1592113802&lat=${lat}&lng=${lng}`
+        );    
+    }
+}
