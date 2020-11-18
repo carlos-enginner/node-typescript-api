42c07e4e7612c2720553efb3bdea8c273b4ff622
Carlos Augusto <gyn.carlos@gmail.com>
2020-11-17 21:20:01 -0300

    Conclusão do C04P03

--- test/functional/beaches.test.ts
+++ test/functional/beaches.test.ts
@@ -1,4 +1,9 @@
+import { Beach } from "@src/models/beach";
+
 describe('Beaches functional tests', () => {
+    // Apaga tudo
+    beforeAll(async() => await Beach.deleteMany({}));
+
     describe('When creating a beach', () => {
         it('Shoud create a beach wich success', async() => {
             const newBeach = {
@@ -12,5 +17,27 @@ describe('Beaches functional tests', () => {
             expect(response.status).toBe(201);
             expect(response.body).toEqual(expect.objectContaining(newBeach));
         });
-    })  
+
+        it('shoud return 422 there is a validation error', async() => {
+            const newBeach = {
+                lat: 'invalid_string',
+                lng: 151.14544,
+                name: 'Manly',
+                position: 'E'
+            };
+
+            const response = await global.testRequest.post('/beaches').send(newBeach);
+
+            expect(response.status).toBe(422);
+            expect(response.body).toEqual({
+                error:
+                'Beach validation failed: lat: Cast to Number failed for value "invalid_string" at path "lat"'
+            });
+        });
+        
+        it.skip('should return 500 when there is any error other than validation error', async() => {
+           // TODO think in a way to throw a 500 
+        });
+
+    })
 });
