1cfc48a2b27599e3f305c07173f96c030259d8f0
Carlos Augusto <gyn.carlos@gmail.com>
2020-12-03 19:51:18 -0300

    Implementação final do capítulo C05P05

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,5 +1,5 @@
 import bcrypt from 'bcrypt';
-import jwt, { JsonWebTokenError } from 'jsonwebtoken';
+import jwt from 'jsonwebtoken';
 import config from 'config';
 import { User } from '@src/models/user';
 
