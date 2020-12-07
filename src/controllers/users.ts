e9f21d0f10a9172e8b6aaa5375ac90cb9f9317c2
Carlos Augusto <gyn.carlos@gmail.com>
2020-12-07 13:08:36 -0300

    Implement me in users endpoint

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,8 +1,9 @@
-import { Controller, Post } from "@overnightjs/core";
+import { Controller, Post, Get, Middleware } from "@overnightjs/core";
 import {Response,Request} from "express";
 import {User} from "@src/models/user";
 import { BaseController } from ".";
 import AuthService from "@src/services/auth";
+import { authMiddleware } from "@src/middlewares/auth";
 
 @Controller('users')
 export class UsersControler extends BaseController {
@@ -31,5 +32,20 @@ export class UsersControler extends BaseController {
 
         const token = AuthService.generateToken(user.toJSON());
         return res.status(200).send({token:token});
+    }
+
+    @Get('me')
+    @Middleware(authMiddleware)
+    public async me(req: Request, res:Response): Promise<Response> {
+        const email = req.decoded ? req.decoded.email : undefined;
+        const user = await User.findOne({email});
+        if (!user) {
+            return this.sendErrorResponse(res, {
+                code: 404,
+                message: 'User not found!',
+            })
+        }
+
+        return res.send({user});
     }
 }
